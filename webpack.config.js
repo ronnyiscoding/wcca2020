265abf21c9372a68a8edaa3ef1bc860d1882cf48
RONNY FREITES <ronnyangelo.freites@gmail.com>
2020-03-07 17:17:02 -0600

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,24 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const postCssConfig = require('./postcss.config');
 const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
   entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js'
+  },
   plugins: [
+    new HtmlWebpackPlugin({
+      title: 'WordCamp Centro América 2020',
+      template: './src/index.html'
+    }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: 'wcca-styles.css',
